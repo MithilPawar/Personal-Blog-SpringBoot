522194defd26043b4e212e5a4d9fca61822a46dc
Mithil Pawar <mithilpawar7044@gmai.com>
2025-11-11 15:48:57 +0530

    Removed one anotherApi endpoint from user controller

--- src/main/java/com/blog/personal_blog/controller/UserController.java
+++ src/main/java/com/blog/personal_blog/controller/UserController.java
@@ -8,9 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
@@ -27,21 +24,5 @@ public class UserController {
 
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new RuntimeException("User not found"));
-    }
-
-    @GetMapping("/another")
-    public Map<String, Object> getAnother(){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String username = authentication.getName();
-
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new RuntimeException("User not found!"));
-
-        Map<String, Object> userData = new HashMap<>();
-        userData.put("Name", user.getUsername());
-        userData.put("Role", user.getRole());
-        userData.put("Status", user.isEnabled());
-
-        return userData;
     }
 }
